36643a6355b8927fdf1bfe676ac7114de604a4b2
OkanHollanderDM <okan.hollander@datamobility.io>
2024-09-21 21:45:00 +0200

    deleted

--- Creating and Administrating Django Models/inventory/admin.py
+++ Creating and Administrating Django Models/inventory/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Category, Product, ProductType, ProductLine
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name', 'slug']
+    list_display = ['id', 'name', 'slug', 'is_active']
+    list_editable = ['is_active']
+    list_display_links = ['id']
+
+
+class ProductLineInLine(admin.StackedInline):
+    model = ProductLine
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['id', 'name', 'category_link', 'is_active', 'is_digital', 'stock_status']
+    list_editable = ['is_active', 'is_digital', 'stock_status']
+    list_display_links = ['id']
+    search_fields = ['name', 'stock_status']
+    list_filter = ['stock_status']
+
+    def category_link(self, obj):
+        link = reverse('admin:inventory_category_change', args=[obj.category.id])
+        return format_html('<a href="{}">{}</a>', link, obj.category.name)
+
+    category_link.short_description = 'Category'
+
+    inlines = [ProductLineInLine]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductType)
